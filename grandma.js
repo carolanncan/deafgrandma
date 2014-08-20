b4dbbe2e574f802f7a3f30c8b11c82cf4848049c
Carolann Bonner <carolanncan@gmail.com>
2014-08-20 01:46:55 -0700

    Add function to get random year

--- grandma.js
+++ grandma.js
@@ -1,9 +1,15 @@
 var speakUp = "Huh?! Speak up, Sonny!"
-var yellsBack = "No, not since 1938!"
+var yellsBack = "No, not since " + getYear() + "!"
+var min = 1920
+var max = 1945
 
 function deafGrandma(string) {
   if (string.toUpperCase() !== string) {
     return speakUp;
   }
   return yellsBack;
+}
+
+function getYear() {
+  return Math.round(Math.random() * (max - min) + min);
 }
