63cf3151317bde9b96f8cf16818f3c0cacd7e934
Carolann Bonner <carolanncan@gmail.com>
2014-08-20 09:04:03 -0700

    Move year function to variable

--- grandma.js
+++ grandma.js
@@ -1,5 +1,9 @@
+var randomYear = function getRandomYear() {
+  return Math.round(Math.random() * (max - min) + min);
+};
+
 var speakUp = "Huh?! Speak up, Sonny!"
-var yellsBack = "No, not since " + getYear() + "!"
+var yellsBack = "No, not since " + randomYear() + "!"
 var min = 1920
 var max = 1945
 
@@ -8,8 +12,5 @@ function deafGrandma(string) {
     return speakUp;
   }
   return yellsBack;
-}
+};
 
-function getYear() {
-  return Math.round(Math.random() * (max - min) + min);
-}
