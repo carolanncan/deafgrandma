b9a981a086422b7da1c42276f1ce4941cbfda8f3
Carolann Bonner <carolanncan@gmail.com>
2014-08-21 09:51:55 -0700

    Add console to print to screen

--- grandma.js
+++ grandma.js
@@ -1,13 +1,12 @@
 var min = 1920
 var max = 1945
-var randomYear = function getRandomYear() {
+var randomYear = function() {
   return Math.round(Math.random() * (max - min) + min);
 };
 var speakUp = "Huh?! Speak up, Sonny!"
-
 var yellsBack = function() {
   return "No, not since " + randomYear() + "!";
-}
+};
 
 function deafGrandma(string) {
   if (string.toUpperCase() !== string) {
