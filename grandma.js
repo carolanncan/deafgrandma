0082514c403bd7f2f31e4251f8aeb39b9c10c60a
Carolann Bonner <carolanncan@gmail.com>
2014-08-20 22:29:43 -0700

    Fix NaN and variables invocation for frandom year

--- grandma.js
+++ grandma.js
@@ -1,16 +1,21 @@
+var min = 1920
+var max = 1945
 var randomYear = function getRandomYear() {
   return Math.round(Math.random() * (max - min) + min);
 };
-
 var speakUp = "Huh?! Speak up, Sonny!"
-var yellsBack = "No, not since " + randomYear() + "!"
-var min = 1920
-var max = 1945
+
+var yellsBack = function() {
+  return "No, not since " + randomYear() + "!";
+}
 
 function deafGrandma(string) {
   if (string.toUpperCase() !== string) {
     return speakUp;
   }
-  return yellsBack;
+  return yellsBack()
 };
 
+console.log(deafGrandma("HELLO"));
+console.log(deafGrandma("HELLO"));
+console.log(deafGrandma("Hello"));
