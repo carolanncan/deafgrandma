be75d488932a043bf1c70e8eee0463e651222f39
Carolann Bonner <carolanncan@gmail.com>
2014-09-08 20:49:38 -0700

    Simplify logic of talk.js

--- js/talk.js
+++ js/talk.js
@@ -5,12 +5,12 @@ process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', function() {
   var sentence = processInput(process.stdin.read());
+  var grandmaResponse = (grandma.interface(sentence) + "\n");
 
-  if (grandma.interface(sentence) === grandma.byeResponse) {
-    process.stdout.write(grandma.byeResponse);
+  process.stdout.write(grandmaResponse);
+
+  if (grandmaResponse === grandma.byeResponse) {
     process.exit();
   }
-
-  process.stdout.write(grandma.interface(sentence) + "\n");
 
 });
