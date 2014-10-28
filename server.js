b16e580266fda66d99867cec50359634be632457
Carolann Bonner <carolanncan@gmail.com>
2014-10-27 22:42:52 -0700

    Server wip

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 var app = connect();
 
+require."./application/grandma"
+
 app.use(serveStatic('application'));
 app.listen(3000);
 
